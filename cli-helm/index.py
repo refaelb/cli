0afc761afa63aee5f393619c8226a896a001750b
refael test <refael058325@gmail.com>
2022-03-30 06:54:26 +0000

    update functions in  index.py

--- cli-helm/index.py
+++ cli-helm/index.py
@@ -8,9 +8,6 @@ import yaml
 import re
 import inquirer
 
-
-
-
 values = """
 name: {}
 #fullname:
@@ -28,11 +25,9 @@ ingress:
   enabled: {}
   hosts:
   - host: {}
-    paths:
-      - path: {}
-        service: {}
-        port: {}
+    paths:"""
 
+dataservice="""
 service:
   type: ClusterIP
   ports: """
@@ -41,6 +36,54 @@ dataService = """
   - name: {}
     port: {}
     """
+dataingress = """
+    - path: {}
+        service: {}
+        port: {}
+"""
+
+def ingressFunc( imagename, replicacount, pullsecret, repo, tag ,configmap, ingress):
+    print (ingress)
+    if ingress == True:
+        questions = [
+        inquirer.Text("ingresshost", message="host to ingress " ),
+    ]        
+        answers = inquirer.prompt(questions)
+        ingresshost = answers['ingresshost']
+    else:
+        ingress = False
+        ingressHost = "nun"
+    with open(imagename+'-values.yaml', 'w') as yfile:
+        yfile.write(values.format(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,ingressHost))
+
+def ingressWrite(imagename, dataingress,numberpath):
+    for i in range (numberpath):
+      questions = [
+      inquirer.Text("ingresspath", message="path to ingress " ),
+      inquirer.Text("ingressport", message="port to ingress " ),
+      inquirer.Text("ingressservice", message="service to ingress " )
+      ]        
+      answers = inquirer.prompt(questions)
+      ingresspath = answers['ingresspath']
+      ingressport = answers['ingressport']
+      ingressservice = answers['ingressservice']
+      with open(imagename+'-values.yaml', 'a') as yfile:
+          yfile.write(dataingress.format(ingresspath, ingressservice, ingressport))
+
+def serviceWriteStatic(dataservice, imagename):
+    with open(imagename+'-values.yaml', 'a') as yfile:
+        yfile.write(dataservice)
+
+def serviceCountFunc (imagename, counts):
+    for i in range(counts):
+      questions = [
+      inquirer.Text("name", message="service name"),
+      inquirer.Text("port", message="service port" )]
+      answers = inquirer.prompt(questions)
+      name = answers['name']
+      port = answers['port']
+      with open(imagename+'-values.yaml', 'a') as yfile:
+        yfile.write(dataService.format(name, port))
 
 
 @click.command()
@@ -52,60 +95,19 @@ dataService = """
 @click.option('--tag', default="latest",prompt='tag to service',help='The person to greet.')
 @click.option('--configmap',default=[], prompt='Your configmap',help='The person to greet.')
 ###service###
+@click.echo('service configure')
 @click.option('--counts',default=1, prompt='counts service',help='The person to greet.')
 ###ingress###
+@click.echo('ingres configure')
 @click.option('--ingress', default=False, prompt='###ingress### \ningress: true or false',help='The person to greet.')
+@click.option('--numberpath',default=1,  prompt='number path ',help='The person to greet.')
 
 
-def main(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,counts):
-    ingressfunc(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress)
-    # write(imagename, replicacount, pullsecret, repo, tag ,configmap,ingress,myarray)
-    countfunc(imagename, counts)
-
-def ingressfunc( imagename, replicacount, pullsecret, repo, tag ,configmap, ingress):
-    print (ingress)
-    if ingress == True:
-        questions = [
-        inquirer.Text("ingresshost", message="host to ingress " ),
-        inquirer.Text("ingresspath", message="path in service to ingress?"),
-        inquirer.Text("ingressport", message="port in service to ingress " ),
-        inquirer.Text("ingressservice", message="service name to ingress" )
-    ]        
-        answers = inquirer.prompt(questions)
-        ingresshost = answers['ingresshost']
-        ingresspath = answers['ingresspath']
-        ingressport = answers['ingressport']
-        ingressservice = answers['ingressservice']
-    else:
-        ingress = False
-        ingresshost = "nun"
-        ingresspath = "nun"
-        ingressport = "nun"
-        ingressservice = "nun"
-    with open(imagename+'-values.yaml', 'w') as yfile:
-        yfile.write(values.format(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,ingresshost,ingresspath,ingressservice,ingressport))
-
-        # return ingress , ingresshost, ingresspath, ingressport , ingressservice
-
-def write(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,myarray):
-    with open(imagename+'-values.yaml', 'w') as yfile:
-        ingresspath = myarray[1]
-        ingressport = myarray[2]
-        ingresshost = myarray[3]
-        ingressservice = myarray[4]
-        yfile.write(values.format(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,ingresshost,ingresspath,ingressservice,ingressport))
-
-def countfunc (imagename, counts):
-    for i in range(counts):
-      questions = [
-      inquirer.Text("name", message="service name"),
-      inquirer.Text("port", message="service port" )
-  ]
-      answers = inquirer.prompt(questions)
-      name = answers['name']
-      port = answers['port']
-      with open(imagename+'-values.yaml', 'a') as yfile:
-        yfile.write(dataService.format(name, port))
+def main(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,counts,numberpath):
+    ingressFunc(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress)
+    ingressWrite(imagename, dataingress, numberpath)
+    serviceWriteStatic(dataservice,imagename)
+    serviceCountFunc(imagename, counts)
 
 
 if __name__ == '__main__':
