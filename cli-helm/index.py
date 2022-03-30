9af0ce757ec0a06403f410b1c89cf4a0225c9087
refael test <refael058325@gmail.com>
2022-03-30 09:42:08 +0000

    update index.yaml

--- cli-helm/index.py
+++ cli-helm/index.py
@@ -20,12 +20,7 @@ images:
 
 config:
   configmaps: {}
-
-ingress:
-  enabled: {}
-  hosts:
-  - host: {}
-    paths:"""
+"""
 
 dataservice="""
 service:
@@ -36,31 +31,56 @@ dataService = """
   - name: {}
     port: {}
     """
+
+ingressBlock = """
+ingress:
+  enabled: {}
+  hosts:
+  - host: {}
+    paths: """
+
 dataingress = """
     - path: {}
         service: {}
         port: {}
 """
+def writhBlockValues(imagename, replicacount, pullsecret, repo, tag ,configmap):
+    with open(imagename+'-values.yaml', 'w') as yfile:
+        yfile.write(values.format(imagename, replicacount, pullsecret, repo, tag ,configmap))
 
-def ingressFunc( imagename, replicacount, pullsecret, repo, tag ,configmap, ingress):
-    print (ingress)
+def serviceWriteStatic(dataservice, imagename):
+    with open(imagename+'-values.yaml', 'a') as yfile:
+        yfile.write(dataservice)
+
+def serviceCountFunc (imagename, counts):
+    for i in range(counts):
+      questions = [
+      inquirer.Text("name", message="service name",default="http"),
+      inquirer.Text("port", message="service port" ,default=80)]
+      answers = inquirer.prompt(questions)
+      name = answers['name']
+      port = answers['port']
+      with open(imagename+'-values.yaml', 'a') as yfile:
+        yfile.write(dataService.format(name, port))
+
+def writeIngerssBlock1(imagename, ingress):
     if ingress == True:
         questions = [
         inquirer.Text("ingresshost", message="host to ingress " ),
     ]        
         answers = inquirer.prompt(questions)
-        ingresshost = answers['ingresshost']
+        ingressHost = answers['ingresshost']
     else:
         ingress = False
         ingressHost = "nun"
-    with open(imagename+'-values.yaml', 'w') as yfile:
-        yfile.write(values.format(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,ingressHost))
+    with open(imagename+'-values.yaml', 'a') as yfile:
+        yfile.write(ingressBlock.format(ingress, ingressHost))
 
-def ingressWrite(imagename, dataingress,numberpath):
+def writeIngressBlock2(imagename, dataingress,numberpath):
     for i in range (numberpath):
       questions = [
-      inquirer.Text("ingresspath", message="path to ingress " ),
-      inquirer.Text("ingressport", message="port to ingress " ),
+      inquirer.Text("ingresspath", message="path to ingress " ,default="/"),
+      inquirer.Text("ingressport", message="port to ingress ", default=3000 ),
       inquirer.Text("ingressservice", message="service to ingress " )
       ]        
       answers = inquirer.prompt(questions)
@@ -70,20 +90,6 @@ def ingressWrite(imagename, dataingress,numberpath):
       with open(imagename+'-values.yaml', 'a') as yfile:
           yfile.write(dataingress.format(ingresspath, ingressservice, ingressport))
 
-def serviceWriteStatic(dataservice, imagename):
-    with open(imagename+'-values.yaml', 'a') as yfile:
-        yfile.write(dataservice)
-
-def serviceCountFunc (imagename, counts):
-    for i in range(counts):
-      questions = [
-      inquirer.Text("name", message="service name"),
-      inquirer.Text("port", message="service port" )]
-      answers = inquirer.prompt(questions)
-      name = answers['name']
-      port = answers['port']
-      with open(imagename+'-values.yaml', 'a') as yfile:
-        yfile.write(dataService.format(name, port))
 
 
 @click.command()
@@ -95,19 +101,20 @@ def serviceCountFunc (imagename, counts):
 @click.option('--tag', default="latest",prompt='tag to service',help='The person to greet.')
 @click.option('--configmap',default=[], prompt='Your configmap',help='The person to greet.')
 ###service###
-@click.echo('service configure')
+# @click.echo('service configure')
 @click.option('--counts',default=1, prompt='counts service',help='The person to greet.')
 ###ingress###
-@click.echo('ingres configure')
+# @click.echo('ingres configure')
 @click.option('--ingress', default=False, prompt='###ingress### \ningress: true or false',help='The person to greet.')
 @click.option('--numberpath',default=1,  prompt='number path ',help='The person to greet.')
 
 
 def main(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress,counts,numberpath):
-    ingressFunc(imagename, replicacount, pullsecret, repo, tag ,configmap, ingress)
-    ingressWrite(imagename, dataingress, numberpath)
+    writhBlockValues(imagename, replicacount, pullsecret, repo, tag ,configmap)
     serviceWriteStatic(dataservice,imagename)
     serviceCountFunc(imagename, counts)
+    writeIngerssBlock1(imagename, ingress)
+    writeIngressBlock2(imagename, dataingress, numberpath)
 
 
 if __name__ == '__main__':
